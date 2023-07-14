94362d18c5099f7da0b6b25f810f1d67545d9c66
@OZ✦LEIS™ <127190727+ozaleis@users.noreply.github.com>
2023-07-14 05:17:09 -0700

    ft_print_comb.c

---
+++ C/C00/EX05/ft_print_comb.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void ft_print_comb(void)
+{
+    char digits[] = "0123456789";
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = i + 1; j < 10; j++)
+        {
+            for (int k = j + 1; k < 10; k++)
+            {
+                write(1, &digits[i], 1);
+                write(1, &digits[j], 1);
+                write(1, &digits[k], 1);
+
+                if (i != 7 || j != 8 || k != 9)
+                {
+                    write(1, ", ", 2);
+                }
+            }
+        }
+    }
+}
