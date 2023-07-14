93fa2c093ab43795d3f0613bd171c801d469323d
@OZ✦LEIS™ <127190727+ozaleis@users.noreply.github.com>
2023-07-14 05:55:22 -0700

    ft_putcha.c

---
+++ C/C00/ft_putcha.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        nb = -nb;
+    }
+
+    if (nb >= 10)
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+    else
+    {
+        ft_putchar(nb + '0');
+    }
+}
